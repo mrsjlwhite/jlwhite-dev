3ece6f902eaf9128d036aa40b269cc5119781940
mrsjlwhite <jlwhite.codes@gmail.com>
2023-02-16 19:47:02 -0800

    ✨ Added nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import React, { useEffect } from "react";
 import './App.scss';
-import AboutMeBanner from './components/about-me-banner/AboutMeBanner';
-import Header from './components/header/Header';
+import AboutMeBanner from './components/AboutMeBanner';
+import Header from './components/Header';
+import MyNavbar from './components/MyNavbar';
 
 function App() {
+
+  useEffect(() => {
+    let url = window.location.href.split("/");
+    let target = url[url.length - 1].toLowerCase();
+    let element = document.getElementById(target);
+    element && element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, []);
+
   return (
     <div>
       <header className="app-header">
         <Header></Header>
       </header>
-      <section className='about-me'>
-        <AboutMeBanner></AboutMeBanner>
+      <MyNavbar></MyNavbar>
+      <AboutMeBanner></AboutMeBanner>
+      <section id='my-experience'>
+        <p>gonna be my experience and stuff</p>
       </section>
-      <section className='my-experience'>
-
-      </section>
-      <section className='my-links'>
-
+      <section id='my-links'>
+        <p>gonna be links to my stuff and things</p>
       </section>
     </div>
   );
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,47 @@
+import '../styles/myNavbar.scss';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function MyNavbar() {
+
+    const navigateToSection = (event, sectionName) => {
+        const section = document.getElementById(sectionName);
+        event.preventDefault();
+        section && section.scrollIntoView({ behavior: "smooth", block: "start" });
+        window.history.pushState(sectionName, sectionName, `/${sectionName}`);
+    }
+
+    return (
+        <Navbar expand="lg" sticky='top' className='my-navbar'>
+            <Container>
+                <Navbar.Brand href="/" className='nav-brand'>J.L.W</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className='nav-links-container'>
+                    <Nav>
+                        <Nav.Link
+                            className='my-nav-link'
+                            href="/"
+                            onClick={(e) => navigateToSection(e, 'about-me')}>
+                            About
+                        </Nav.Link>
+                        <Nav.Link
+                            className='my-nav-link'
+                            href="/"
+                            onClick={(e) => navigateToSection(e, 'my-experience')}>
+                            Experience
+                        </Nav.Link>
+                        <Nav.Link
+                            className='my-nav-link'
+                            href="/"
+                            onClick={(e) => navigateToSection(e, 'my-links')}>
+                            Contact
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default MyNavbar;
