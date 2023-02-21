5bca6ba3cf7cab1b2923769f75a0982d3dfded64
mrsjlwhite <jlwhite.codes@gmail.com>
2023-02-20 16:05:50 -0800

    📱 Updated gallery to be responsive, refactored to individual components, updated packages

--- src/components/ExperiencesGallery.js
+++ src/components/ExperiencesGallery.js
@@ -1,126 +1,73 @@
 import '../styles/experiencesGallery.scss';
 import React, { useEffect, useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import Container from 'react-bootstrap/Container';
 import JobExperiences from '../data/jobExperiences';
-import { ArrowRight, ArrowLeft } from 'react-bootstrap-icons';
-import { Triangle } from 'react-loader-spinner';
+import { useMediaQuery } from 'react-responsive';
+import Carousel from 'react-bootstrap/Carousel';
+import ExperienceCard from './ExperienceCard';
+import LoadingIcon from './LoadingIcon';
 
 function ExperiencesGallery() {
     const [jobs, setJobs] = useState([]);
-    const [firstCardIndex, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
+
+    const isMobile = useMediaQuery({ query: `(max-width: 650px)` });
 
     useEffect(() => {
         const jobs = Array.from(JobExperiences);
 
-        // const firstJobIndex = indexes[0];
-        // const secondJobIndex = indexes[1] + 1;
-        // const secondCardIndex = firstCardIndex + 2 > jobs.length ? firstCardIndex + 1 : firstCardIndex + 2;
-
-        // const visibleCards = jobs.slice(firstCardIndex, secondCardIndex).map((job) => {
-        //     job.style = 'show ';
-        //     return job;
-        // });
-        // const hiddenCards = jobs.splice(secondCardIndex).map((job) => {
-        //     job.style = 'hide ';
-        //     return job;
-        // });
-
-        const visibleCard = jobs[firstCardIndex];
-        visibleCard.style = 'show ';
-
-        const hiddenCards = jobs.filter((job, index) => index !== firstCardIndex).map((job) => {
-            job.style = 'hide ';
-            return job;
-        });
-
-        if (!visibleCard || !hiddenCards) {
-            return setJobs(JobExperiences);
+        if (isMobile) {
+            return setJobs(jobs);
         }
 
-        setJobs([...hiddenCards]);
+        const jobPairs = jobs.reduce((accumulator, currentValue, currentIndex, array) => {
+            if (currentIndex % 2 === 0) {
+                accumulator.push(array.slice(currentIndex, currentIndex + 2));
+            }
+            return accumulator;
+        }, []);
 
-        setTimeout(() => {
-            setJobs([visibleCard, ...hiddenCards]);
-        }, 1000);
+        setJobs(jobPairs);
+    }, [isMobile]);
 
-    }, [firstCardIndex]);
-
-    const handlePrevious = () => {
-        const newIndex = firstCardIndex - 1;
-        const newFirstIndex = newIndex < 0 ? jobs.length - 1 : newIndex;
-        setIndex(newFirstIndex - 1);
-    };
-
-    const handleNext = () => {
-        const newIndex = firstCardIndex + 1;
-        const newFirstIndex = newIndex >= jobs.length ? 0 : newIndex;
-        setIndex(newFirstIndex);
-    };
+    const handleSelect = (selectedIndex) => setIndex(selectedIndex);
 
     return (
-        <section id='my-experience' className='experience-section'>
+        <section id='my-experience'>
             <Container>
                 <h4 className='section-title'>my experience.</h4>
-                <div className='section-body'>
-                    <ArrowLeft
-                        size={50}
-                        className='arrow-icon'
-                        onClick={handlePrevious} />
-                    <CardGroup>
-                        {!jobs || !jobs.length
-                            ?
-                            <div className='loading-icon'>
-                                <Triangle
-                                    height="150"
-                                    width="150"
-                                    color="#4fa94d"
-                                    ariaLabel="triangle-loading"
-                                    visible={true}
-                                />
-                            </div>
-                            :
-                            jobs.map((job, index) => {
+
+                {!jobs || !jobs.length
+                    ?
+                    <div className='loading-icon-container'>
+                        <LoadingIcon></LoadingIcon>
+                    </div>
+                    :
+                    <Carousel activeIndex={index} onSelect={handleSelect} fade indicators={false}>
+                        {isMobile
+                            ? jobs.map((job, index) => {
                                 return (
-                                    <Card key={index} id={index} className={job.style + 'experience-card'}>
-                                        <Card.Body>
-                                            <Card.Title className={job.style + 'card-title'}>
-                                                {job.name}
-                                            </Card.Title>
-                                            <div className='card-text-container'>
-                                                <Card.Text className={job.style + 'card-desc'}>
-                                                    {job.description}
-                                                </Card.Text>
-                                                <div>
-                                                    <h5 className='list-title'>Tech Used</h5>
-                                                    <hr />
-                                                    <ul className={job.style}>
-                                                        {job.tech.map((tech, index) => {
-                                                            return (
-                                                                <li key={index}>
-                                                                    <Card.Text>
-                                                                        {tech}
-                                                                    </Card.Text>
-                                                                </li>
-                                                            );
-                                                        })}
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
+                                    <Carousel.Item key={index}>
+                                        <div className='card-container'>
+                                            <ExperienceCard job={job}></ExperienceCard>
+                                        </div>
+                                    </Carousel.Item>
                                 )
                             })
-                        }
-                    </CardGroup>
-                    <ArrowRight
-                        size={50}
-                        className='arrow-icon'
-                        onClick={handleNext} />
-                </div>
-            </Container>
-        </section>
+                            : jobs.map((jobPair, index) => {
+                                return (
+                                    <Carousel.Item key={index}>
+                                        <div className='card-container'>
+                                            <ExperienceCard job={jobPair[0]}></ExperienceCard>
+                                            <ExperienceCard job={jobPair[1]}></ExperienceCard>
+                                        </div>
+                                    </Carousel.Item>
+                                )
+                            })}
+                    </Carousel>
+                }
+            </Container >
+        </section >
     )
 }
 
