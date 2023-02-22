4f562d2880458493e9af2b0b9ae9b9eb1f43fd36
mrsjlwhite <jlwhite.codes@gmail.com>
2023-02-21 16:04:07 -0800

    🚧 added cloud animation, buggy layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,31 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Header() {
     return (
-        <header className="header">
-            <img className='header-img' src={header} alt='Bright blue skies with a green forest, and a Westfalia hidden between the trees.' />
-            <div className='text-container'>
-                <span className="header-name">Jeslyn Leon White</span>
-                <TypeAnimation
-                    sequence={[
-                        'developer', // Types
-                        2000, // Waits
-                        'engineer', // Deletes 'One' and types 'Two'
-                        2000, // Waits 2s
-                        'coffee addict',
-                        3000,
-                        'driven',
-                        2000
-                    ]}
-                    wrapper="div"
-                    cursor={true}
-                    repeat={Infinity}
-                    className='header-meta'
-                />
-            </div>
-        </header>
+        <>
+            <div className="hero-clouds"></div>
+            <header className="header">
+                <img className='header-img' src={header} alt='Bright blue skies with a green forest, and a Westfalia hidden between the trees.' />
+                <div className='text-container'>
+                    <span className="header-name">Jeslyn Leon White</span>
+                    <TypeAnimation
+                        sequence={[
+                            'developer', // Types
+                            2000, // Waits
+                            'engineer', // Deletes 'One' and types 'Two'
+                            2000, // Waits 2s
+                            'coffee addict',
+                            3000,
+                            'driven',
+                            2000
+                        ]}
+                        wrapper="div"
+                        cursor={true}
+                        repeat={Infinity}
+                        className='header-meta'
+                    />
+                </div>
+            </header>
+        </>
     );
 };
 
