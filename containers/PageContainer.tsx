3b5bca7476890c7a2ac2b425c36233d391273203
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-03-19 17:25:51 -0700

    ✨ Added sub titles to pageContainer

--- containers/PageContainer.tsx
+++ containers/PageContainer.tsx
@@ -4,24 +4,31 @@ import { useEffect, useState } from 'react';
 
 const PageContainer = (props) => {
     const [pageTitle, setPageTitle] = useState<string>('');
+    const [pageDesc, setPageDesc] = useState<string>('');
     const router = useRouter();
 
     useEffect(() => {
         const url = router.asPath.replace('/', '').toLowerCase();
-        const isDetailsPage = router.query && router.query.slug ? 'work details.' : '';
-
+        const isDetailsPage = router.query && router.query.slug;
+        const defaultTitle = `my ${url}s.`;
         let title = '';
+        let desc = '';
 
         switch (url) {
             case 'about':
-                title = `${url} me.`
+                title = `${url} me.`;
+                break;
+            case 'experience':
+                title = defaultTitle;
+                desc = 'an overview of my previous work experiences.';
                 break;
             default:
-                title = isDetailsPage ? isDetailsPage : `my ${url}s.`
+                title = isDetailsPage ? 'work details.' : defaultTitle;
                 break;
         }
 
         setPageTitle(title);
+        setPageDesc(desc);
     }, [router.asPath])
 
     return (
@@ -31,9 +38,14 @@ const PageContainer = (props) => {
                 <div className='container'>
                     <div className='row'>
                         <div className='col'>
-                            <h4 className='section-title mt-5'>
-                                {pageTitle}
-                            </h4>
+                            <div className='d-flex justify-content-between align-items-center mt-5'>
+                                <h4 className='section-title'>
+                                    {pageTitle}
+                                </h4>
+                                <p className='section-sub-title'>
+                                    {pageDesc}
+                                </p>
+                            </div>
                             {props.children}
                         </div>
                     </div>
