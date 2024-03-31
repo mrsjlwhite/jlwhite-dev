87e9dac84231320b8dc1612a8c529f20ba6133ed
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-03-31 11:45:12 -0700

    💄 Restyled about page, uses data file

--- data/aboutMe.ts
+++ data/aboutMe.ts
@@ -1,3 +1,20 @@
-export const aboutMe = `
-    About me words 
-`
+import { IAboutMeBlurb } from "@/interfaces/aboutMeBlurb";
+
+export const aboutMe: IAboutMeBlurb[] = [
+    {
+        title: 'hi!',
+        content: 'In case you care to know a little more about me. Thank you for visiting my site🤍'
+    },
+    {
+        title: 'how I got here.',
+        content: `I graudated in 2014 with an English degree, uncertain of a career path ahead.
+        But after discovering a coding bootcamp, I quickly became entraced in the world of programming.
+        Since graduating my coding bootcamp in 2017, I have never been able to look back since.`
+    }, 
+    {
+        title: 'history.',
+        content: `Before becoming a developer, I worked in Marketing where it was easy to see 
+        the importance of communication between creatives and engineers, continously trying to understand 
+        the best way to bridge both.`
+    }
+]
