796246f0502aa97bcdfd47ef5c4b75ed2f4ebc23
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-04-15 21:17:11 -0700

    💄 Updated aboutMe for gallery changes

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,6 +5,7 @@ import TechIcon from '@/components/shared/techIcon/TechIcon';
 import { tldr } from '@/data/tldrExperience';
 import { IAboutMeBlurb } from '@/interfaces/aboutMeBlurb';
 import AboutMeGallery from '@/components/aboutMe/AboutMeGallery';
+import { GalleryType } from 'constants/galleryType';
 
 type Props = {
     blurbs: IAboutMeBlurb[]
@@ -15,7 +16,6 @@ const About = ({ blurbs, techIcons }: Props) => {
 
     return (
         <PageContainer>
-            <AboutMeGallery />
             {blurbs.map((blurb) => {
                 return (
                     <section key={blurb.title}>
@@ -31,6 +31,7 @@ const About = ({ blurbs, techIcons }: Props) => {
                     </section>
                 )
             })}
+            <AboutMeGallery photoSet={GalleryType.SET2} />
             <div className={styles.iconFooter}>
                 {techIcons.map((icon) => <TechIcon key={icon} icon={icon} className='m-1' />)}
             </div>
