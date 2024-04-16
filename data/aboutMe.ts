4b724aa5e80f9459770fb8ac69ba86f847006e88
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-04-15 20:40:28 -0700

    🌱 Updated aboutMe content

--- data/aboutMe.ts
+++ data/aboutMe.ts
@@ -3,18 +3,37 @@ import { IAboutMeBlurb } from "@/interfaces/aboutMeBlurb";
 export const aboutMe: IAboutMeBlurb[] = [
     {
         title: 'hi!',
-        content: 'In case you care to know a little more about me. Thank you for visiting my site🤍'
+        content: `Thank you for visiting my site 🤍`
+    },
+    {
+        title: 'intros.',
+        content: `I'm a Latina Fullstack Software Developer, who loves building web and mobile applications. 
+        My goal is to build clean, easy-to-use, and aestheticly-pleasing applications, while also staying within good coding practices. 
+      `
     },
     {
         title: 'how I got here.',
-        content: `I graudated in 2014 with an English degree, uncertain of a career path ahead.
-        But after discovering a coding bootcamp, I quickly became entraced in the world of programming.
-        Since graduating my coding bootcamp in 2017, I have never been able to look back since.`
-    }, 
+        content: `I graduated with an English degree, and shortly found myself in a masters program I was unhappy in.
+        It wasn't until I was introduced to a coding bootcamp, the same one that helped me start working in a startup environment
+        in the bootcamp, that I became enthralled in the world of programming and haven't looked back since.`
+    },
     {
-        title: 'history.',
-        content: `Before becoming a developer, I worked in Marketing where it was easy to see 
-        the importance of communication between creatives and engineers, continously trying to understand 
-        the best way to bridge both.`
+        title: 'developer.',
+        content: `I've been doing fullstack web development work since 2017, working in larger and smaller teams.
+        I've worked closely with designers and product owners to match mockup expectations, while also meeting the techical
+        requirements and constraints of my team. I've happily been working fully remote since 2020, 
+        where my favorite kind of day is a fresh cup of coffee and hours upon hours of coding ahead of me - talk about bliss ☕️`
+    },
+    {
+        title: 'diverse experience.',
+        content: `Before becoming a developer, I worked in Marketing where I quickly experience
+        the importance of communication between creatives and engineers, and the importance of continously trying to understand 
+        the best way to bridge that communication.`
+    },
+    {
+        title: 'fun.',
+        content: `In my free time, I enjoy books, films and video games. 
+        I love the art of story telling they each uniquely bring, the aesthetic and world building they establish that can take you elsewhere,
+        the color art that's presented. I aspire to bring elements of these inspirations in my own work.`
     }
 ]
