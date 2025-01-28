4da3ae85a13861721befe80a1e8470c6f0d4bc27
Jeslyn <jlwhite.codes@gmail.com>
2025-01-27 19:10:25 -0800

    💚 Fixes about page loading, removed imgs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,32 @@
+'use client'
 import styles from './about.module.scss';
 import { aboutMe } from 'core/data/aboutMe';
 import PageContainer from 'components/containers/PageContainer';
 import TechIcon from 'components/techIcon/TechIcon';
-import AboutMeGallery from 'components/aboutMeGallery/AboutMeGallery';
-import { GalleryType } from 'lib/constants';
+import Image from 'next/image';
+import jobTechIcons from '@/data/jobTechIcons';
+import { useEffect, useState } from 'react';
 
+const photoSet = [
+    { img: '/imgs/fun1.jpeg', text: 'Wildflower from a hike' },
+    { img: '/imgs/fun2.jpg', text: 'Working in a lodge' },
+    { img: '/imgs/fun3.jpeg', text: 'Working by the beach' }
+]
 
 const About = () => {
+    const [techIcons, setTechIcons] = useState([]);
+    const [photos, setPhotos] = useState([]);
     const blurbs = aboutMe;
-    // const techIcons = aboutMe
+
+    useEffect(() => {
+        if (techIcons.length === 0) {
+            setTechIcons(jobTechIcons.get('TL;DR'));
+        }
+        if (photos.length === 0) {
+            setPhotos(photoSet);
+        }
+    }, []);
+
     return (
         <PageContainer>
             {blurbs.map((blurb) => {
@@ -26,10 +44,23 @@ const About = () => {
                     </section>
                 )
             })}
-            <AboutMeGallery photoSet={GalleryType.SET2} />
-            {/* <div className={styles.iconFooter}>
+
+            <div className={styles.aboutMeGallery}>
+                {photoSet.map(({ img, text }) => {
+                    return (
+                        <Image
+                            src={img}
+                            alt={text}
+                            key={text}
+                            height={350}
+                            width={385} />
+                    )
+                })}
+            </div>
+
+            <div className={styles.iconFooter}>
                 {techIcons.map((icon) => <TechIcon key={icon} icon={icon} className='m-1' />)}
-            </div> */}
+            </div>
         </PageContainer>
     )
 }
