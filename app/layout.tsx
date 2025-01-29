e3b3d6edc6e60b3b7a638c3a8c2e8eb4a2d9ceb5
Jeslyn <jlwhite.codes@gmail.com>
2025-01-28 18:14:02 -0800

    📦 Fixes bootstrap JS package, updated current refs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import MyFooter from '@/components/myFooter/MyFooter';
+'use client'
+
+import MyFooter from '@/components/myFooter/MyFooter'
 import '@/styles/global.scss'
-import { Lora, Open_Sans } from 'next/font/google';
+import { Lora, Open_Sans } from 'next/font/google'
 import localFont from 'next/font/local'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
 
 // Font files can be colocated inside of `app`
 const titleFont = localFont({
@@ -25,11 +29,14 @@ export default function RootLayout({
 }: {
     children: React.ReactNode
 }) {
+
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }, []);
+
     return (
         <html lang="en" className={`${titleFont.variable} ${subHeadingFont.variable} ${bodyFont.variable}`}>
             <head>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-                    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
             </head>
             <body>
