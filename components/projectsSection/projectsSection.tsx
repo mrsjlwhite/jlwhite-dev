ccce322ec4afc48d70ba724ca02a9e280de09d0f
Jeslyn <jlwhite.codes@gmail.com>
2025-02-04 01:06:45 -0800

    ✨ Homepage components fadeIn on scroll

--- components/projectsSection/projectsSection.tsx
+++ components/projectsSection/projectsSection.tsx
@@ -1,10 +1,11 @@
 'use client'
 import Link from 'next/link'
 import styles from './projectsSection.module.scss'
-import { useEffect, useState } from 'react';
-import { IWorkProject } from '@/interfaces/workProject';
-import ProjectCover from 'components/projectCover/projectCover';
-import SectionHeader from '../sectionHeader/sectionHeader';
+import { useEffect, useState } from 'react'
+import { IWorkProject } from '@/interfaces/workProject'
+import ProjectCover from 'components/projectCover/projectCover'
+import SectionHeader from '../sectionHeader/sectionHeader'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 const ProjectsSection = ({ projects }) => {
     const [workProjects, setWorkProjects] = useState<IWorkProject[]>([]);
@@ -20,16 +21,18 @@ const ProjectsSection = ({ projects }) => {
     return (
         <section id="my-work" className='home-section'>
             <SectionHeader title='Some of My Projects' />
-            <div className={styles.projectsContainer}>
-                <div className={styles.imgGrid}>
-                    {workProjects.map((project: IWorkProject, index: number) =>
-                        <div key={index} className={styles.imgGridItem}>
-                            <ProjectCover slug={project.slug} name={project.name} />
-                        </div>
-                    )}
+            <AnimationOnScroll animateIn="animate__fadeIn">
+                <div className={styles.projectsContainer}>
+                    <div className={styles.imgGrid}>
+                        {workProjects.map((project: IWorkProject, index: number) =>
+                            <div key={project.name} className={styles.imgGridItem}>
+                                <ProjectCover slug={project.slug} name={project.name} />
+                            </div>
+                        )}
+                    </div>
                 </div>
-            </div>
-            <Link href='/projects' className={styles.viewAllLink}>View All</Link>
+                <Link href='/projects' className={styles.viewAllLink}>View All</Link>
+            </AnimationOnScroll>
         </section>
     )
 }
